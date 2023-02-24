a6793d392f9a39796af5971a633395615fc14ad6
muhamadoskhar <muhamad.oskhar22@mhs.uinjkt.ac.id>
2023-02-25 02:36:01 +0700

    editing menggunakan GNU IMAGE

--- app/views/shared/widgets/header.php
+++ app/views/shared/widgets/header.php
@@ -9,7 +9,6 @@ if (!isset($controller))
 
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="<?= $dataConfig["LOGO"] ?>" alt="iniGambar" rel="icon" type="image/x-icon">
 <meta property="og:description" content="<?= $controller->pageDecs ?>">
 <meta property="og:image" content="assets/icons/<?= $controller->pageImage ?>" alt="iniGambar">
