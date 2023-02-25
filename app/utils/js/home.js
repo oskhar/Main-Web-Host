e6255c3e0eac6fa4513d5648f2150d6c26f7773c
muhamadoskhar <muhamad.oskhar22@mhs.uinjkt.ac.id>
2023-02-25 18:26:44 +0700

    menyesuaikan meteor

--- app/utils/js/home.js
+++ app/utils/js/home.js
@@ -1,21 +1,24 @@
-console.log("terinput");
-window.createAst = function() {
-    const meteor = document.createElement('i');
-    var node = document.createTextNode(".");
-    meteor.appendChild(node);
-    meteor.style.top = Math.random() * 600 + 'px';
-    meteor.style.opacity = Math.random() + 0.2;
-    meteor.style.fontSize = Math.random() * 30 + 20 + 'px';
-    meteor.style.fontWeight = 'bold';
-    meteor.style.animation = 'meteoriq ' + (Math.random() * 4 + 8) + 's infinite';
-    meteor.style.color = "white";
-    if (meteor.style.opacity < 0.7)
-        meteor.style.zIndex = 1;
-    else
-        meteor.style.zIndex = 5;
-    document.main.appendChild(meteor);
+window.createAst = function(n) {
+    for (let i = 0; i < n; i++) {
+
+        const meteor = document.createElement('i');
+        var node = document.createTextNode(".");
+        meteor.appendChild(node);
+        meteor.style.position = "absolute";
+        meteor.style.top = (Math.random() * innerHeight-100) + 'px';
+        meteor.style.left = i > n / 3 ? (Math.random() * innerWidth) + 'px' : "100vw";
+        meteor.style.opacity = Math.random() + 0.2;
+        meteor.style.fontSize = Math.random() * 30 + 20 + 'px';
+        meteor.style.fontWeight = 'bold';
+        meteor.style.animation = 'meteoriq ' + (Math.random() * 4 + 2) + 's linear infinite';
+        meteor.style.color = "white";
+        if (meteor.style.opacity < 0.7)
+            meteor.style.zIndex = 1;
+        else
+            meteor.style.zIndex = 5;
+        document.querySelector('main').appendChild(meteor);
+
+    }
 }
-for (let i = 0; i < 20; i++) {
-    createAst();
-    
-}
+
+createAst(30);
